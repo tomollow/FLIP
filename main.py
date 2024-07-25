baa3966f9c23ddc3173744089666688ce503c1da
shoji <62646773+tomollow@users.noreply.github.com>
2024-07-25 15:13:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,12 @@ def main():
         if st.button("エンベッディングを実行"):
             load_and_embed_data(tmp_file_path, tmp_file_ext)
         
+        user_api_key = st.text_input(
+            label="OpenAI API key",
+            placeholder="Paste your openAI API key",
+            type="password"
+        )
+        os.environ['OPENAI_API_KEY'] = user_api_key
         select_model = st.selectbox("Model", ["gpt-3.5-turbo-1106", "gpt-4-1106-preview",])
         select_temperature = st.slider("Temperature", min_value=0.0, max_value=2.0, value=0.0, step=0.1,)
         select_chunk_size = st.slider("Chunk", min_value=0.0, max_value=1000.0, value=300.0, step=10.0,)
